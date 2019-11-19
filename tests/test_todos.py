e09d7403c7d425d97b6f3b65848599541acf2e4a
Adam Bolte <adam.bolte@sitepoint.com>
2019-11-19 17:08:24 +1100

    Mocking External APIs in Python, part 4
    https://realpython.com/testing-third-party-apis-with-mocks/
    
    * Mocking the complete service behavior

--- tests/test_todos.py
+++ tests/test_todos.py
@@ -2,50 +2,42 @@
 from unittest.mock import Mock, patch
 
 # Third-party imports...
-from nose.tools import assert_is_not_none
+from nose.tools import assert_list_equal, assert_is_none
 
 # Local imports...
 from project.services import get_todos
 
 
 @patch('project.services.requests.get')
-def test_getting_todos_with_decorator(mock_get):
-    # Configure the mock to return a response with an OK status code.
-    mock_get.return_value.ok = True
+def test_getting_todos_when_response_is_ok(mock_get):
+    todos = [{
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }]
+
+    # Configure the mock to return a response with an OK status
+    # code. Also, the mock should have a `json()` mothod that returns
+    # a list of todos.
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = todos
 
     # Call the service, which will send a request to the server.
     response = get_todos()
 
-    # If the request is sent successfully, then I expect a response to be returned.
-    assert_is_not_none(response)
+    # If the request is sent successfully, then I expect a response to
+    # be returned.
+    assert_list_equal(response.json(), todos)
 
 
-def test_getting_todos_with_context_manager():
-    with patch('project.services.requests.get') as mock_get:
-        # Configure the mock to return a response with an OK status code.
-        mock_get.return_value.ok = True
-
-        # Call the service, which will send a request to the server.
-        response = get_todos()
-
-    # If the request is sent successfully, then I expect a response to be returned.
-    assert_is_not_none(response)
-
-
-def test_getting_todos_with_patcher():
-    mock_get_patcher = patch('project.services.requests.get')
-
-    # Start patching `requests.get`.
-    mock_get = mock_get_patcher.start()
-
-    # Configure the mock to return a response with an OK status code.
-    mock_get.return_value.ok = True
+@patch('project.services.requests.get')
+def test_getting_todos_when_response_is_not_ok(mock_get):
+    # Configure the mock to not return a response with an OK status code.
+    mock_get.return_value.ok = False
 
     # Call the service, which will send a request to the server.
     response = get_todos()
 
-    # Stop patching `requests.get`.
-    mock_get_patcher.stop()
-
-    # If the request is sent successfully, then I expect a response to be returned.
-    assert_is_not_none(response)
+    # If the response contains an error, I should get no todos.
+    assert_is_none(response)
